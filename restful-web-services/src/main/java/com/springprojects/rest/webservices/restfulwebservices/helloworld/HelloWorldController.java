c1be818b05a0ae33fce971bb698f0277882dae24
cpkopp <cameronpkopp@gmail.com>
2023-12-05 13:53:13 -1000

    accept-language headers

--- restful-web-services/src/main/java/com/springprojects/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/springprojects/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.springprojects.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	//@RequestMapping(method = RequestMethod.GET, path = "/hello-world") 
 	@GetMapping(path = "/hello-world")
@@ -22,6 +32,17 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/path-variable/{name}") //path variable in braces
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name)); 
+	}
+	
+	//Internationalized
+	//define values somewhere for different languages, then write code to pick up language
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		
+		Locale locale = LocaleContextHolder.getLocale();
+		
+		//going to handle returned message based on our message.properties files from sent Accept-Language headers
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
 	}
 	
 }
