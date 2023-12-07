7b9b5b6474e93e840cf87e71c85e4575b5a6a122
cpkopp <cameronpkopp@gmail.com>
2023-12-07 09:44:33 -1000

    static filtering examples

---
+++ restful-web-services/src/main/java/com/springprojects/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -0,0 +1,27 @@
+package com.springprojects.rest.webservices.restfulwebservices.filtering;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+	
+	@GetMapping("/filtering")
+	public SomeBean filtering() {
+		
+		return new SomeBean("val1", "val2", "val3");
+		
+	}
+	
+	@GetMapping("/filtering-list")
+	public List<SomeBean> filteringList() {
+		
+		return Arrays.asList(new SomeBean("val1", "val2", "val3"),
+				new SomeBean("val4", "val5", "val6"));
+		
+	}
+	
+}
