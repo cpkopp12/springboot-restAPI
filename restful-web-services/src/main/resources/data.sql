a9318cf2e7e351e2f736c7966a675c8cb47917b2
cpkopp <cameronpkopp@gmail.com>
2023-12-15 19:42:58 -1000

    h2 db

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO user_details (id,birthdate,name)
+VALUES (10001, current_date(), 'Cam');
+
+INSERT INTO user_details (id,birthdate,name)
+VALUES (10002, current_date(), 'Mary');
+
+INSERT INTO user_details (id,birthdate,name)
+VALUES (10003, current_date(), 'Ranga');
